8ca185db940acbeeffb6916c524fb2278e90bed7
Afik799 <108685956+Afik799@users.noreply.github.com>
2022-08-18 17:16:37 +0300

    Update backend_testing.py

--- backend_testing.py
+++ backend_testing.py
@@ -1,7 +1,7 @@
 import requests
 import db_connector
 
-user_id = 1234
+user_id = 789
 user_name = "TomandJerry"
 user_json = {"user_name": f"{user_name}"}
 res = requests.post(url=f"http://0.0.0.0:5000/users/{int(user_id)}", json=user_json)
